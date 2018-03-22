fdb95c2c42279d2abd36753d612024a9e4ee4f14
李正先 <hzlizx@tairanchina.com>
2018-03-22 13:12:50 +0800

    显示语言聚合数据

---
+++ pages/statistics/statistics.js
@@ -0,0 +1,47 @@
+// pages/statistics/statistics.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    language: [],
+    username: "lzx2005"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getLanguageInfo();
+  },
+  getLanguageInfo() {
+    var that = this
+    var data = {
+      username: this.data.username
+    }
+    wx.request({
+      url: 'https://lzx2005.com/github/language',
+      data,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        if (res.data.code == 200) {
+          that.setData({
+            language: res.data.data
+          })
+          console.log(that.data.language);
+        } else {
+          wx.showModal({
+            content: res.data.msg,
+            showCancel: false,
+            success: function (res) {
+            }
+          });
+        }
+      }
+    })
+  }
+})
